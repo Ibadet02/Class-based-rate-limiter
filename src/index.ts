a91eac251cf73c3e496a377088b62334e649fe24
Ibadet02 <ibadetismayilov20@gmail.com>
2026-01-13 22:40:59 +0100

    complete the implementation of the rate limiter

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,19 @@ class RateLimiter {
 
   check(userId: string) {
     const currentTime = Date.now();
+    const currentUser = this.#requests.get(userId);
 
-    if (!this.#requests.has(userId)) {
+    if (!currentUser || currentTime - currentUser.startTime > this.#windowMs) {
       this.#requests.set(userId, { count: 1, startTime: currentTime });
 
       return true;
     }
 
-    const currentUser = this.#requests.get(userId)!;
-
-    if (this.#limit === currentUser.count) {
+    if (this.#limit <= currentUser.count) {
       return false;
     }
 
-    if (currentTime - currentUser.startTime > this.#windowMs) {
-      this.#requests.set(userId, { count: 1, startTime: currentTime });
-
-      return true;
-    }
-
-    this.#requests.set(userId, {
-      ...currentUser,
-      count: currentUser.count + 1,
-    });
-
+    currentUser.count++;
     return true;
   }
 
@@ -42,15 +31,7 @@ class RateLimiter {
   }
 
   clear(userId: string) {
-    const currentUser = this.#requests.get(userId);
-
-    if (!currentUser) {
-      return false;
-    }
-
-    const currentTime = Date.now();
-
-    this.#requests.set(userId, { count: 0, startTime: currentTime });
+    return this.#requests.delete(userId);
   }
 }
 
@@ -65,11 +46,9 @@ limiter.check("user_1");
 limiter.check("user_1");
 limiter.check("user_1");
 limiter.clear("user_1");
-limiter.check("user_1")
+limiter.check("user_1");
 
 setTimeout(() => {
-  limiter.check("user_2");
+  limiter.check("user_9");
   limiter.check("user_2");
 }, 3000);
-
-console.log(limiter.stats);
